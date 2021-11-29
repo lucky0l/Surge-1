7d5c9e94ea37e048d9fb5359314d12d3b85a88d3
ATor <atorninjaonline@gmail.com>
2021-11-29 21:31:22 +0800

    update Bilibili

--- BilibiliRegion.js
+++ BilibiliRegion.js
@@ -76,7 +76,7 @@ let $ = nobyda();
 let run = EnvInfo();
 
 async function SwitchRegion(play) {
-	const Group = $.read('BiliArea_Policy') || '📺 DomesticMedia'; //Your blibli policy group name.
+	const Group = $.read('BiliArea_Policy') || 'Bilibili'; //Your blibli policy group name.
 	const CN = $.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
 	const TW = $.read('BiliArea_TW') || 'TW'; //Your Taiwan sub-policy name.
 	const HK = $.read('BiliArea_HK') || 'HK'; //Your HongKong sub-policy name.
