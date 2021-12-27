f16d11a194c4f4d0ddc796cd38244bd917c11c44
ATor <atorninjaonline@gmail.com>
2021-12-27 16:20:08 +0800

    update weather

--- Weather.js
+++ Weather.js
@@ -2,10 +2,10 @@
 // Developed by Hackl0us (https://github.com/hackl0us)
 
 const $ = new Env('AQI-US');
-$.token = 'hackl0us_aqi_token';
+$.token = '9a3f95842f59adf672bf675e1aa6d81e180b6876';
 
 // STEP 1: 前往 https://aqicn.org/data-platform/token/ 注册账户，将申请的 API Token 填入下方
-const aqicnToken = $.getdata($.token) || '9a3f95842f59adf672bf675e1aa6d81e180b6876';
+const aqicnToken = $.getdata($.token) || '';
 
 // STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
 /*
