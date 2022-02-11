f8e622c20bb7fdb0f0d3f4a121120dee8a3c8d44
ATor <atorninjaonline@gmail.com>
2022-02-12 01:00:20 +0800

    update bilibili

--- bilibili-json.js
+++ bilibili-json.js
@@ -83,6 +83,10 @@ if (!body.hasOwnProperty('data')) {
                         // ad_player大视频广告 ad_web_gif大gif广告 ad_web_s普通小广告 ad_av创作推广广告
                         // console.log(`${cardGoto}广告去除)`);
                         return false;
+                    } else if (cardType === 'cm_v1' && ['ad_web_s', 'ad_av', 'ad_web_gif', 'ad_player'].includes(cardGoto)) {
+                        // ad_player大视频广告 ad_web_gif大gif广告 ad_web_s普通小广告 ad_av创作推广广告
+                        // console.log(`${cardGoto}广告去除)`);
+                        return false;
                     } else if (cardType === 'small_cover_v10' && cardGoto === 'game') {
                         // console.log('游戏广告去除');
                         return false;
