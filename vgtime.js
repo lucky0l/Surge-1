7c789b830a808cb3d3ae128f836763584b6c4728
ATor <atorninjaonline@gmail.com>
2022-02-13 15:16:42 +0800

    update vgtime

--- vgtime.js
+++ vgtime.js
@@ -1,7 +1,35 @@
+let url = $request.url;
+let method = $request.method;
 let body = JSON.parse($response.body);
 let notifiTitle = "去广告脚本错误";
-if (body.data == undefined || body.data.ad === undefined) {
-    $notification.post(notifiTitle, "vgtime", "data/ad字段为undefined")
-} else {
-    body.data.ad = null;
+let getMethod = "GET";
+let postMethod = "POST";
+if (url.indexOf("app02.vgtime.com:8080/vgtime-app/api/v2/init/ad.json") != -1 && method == postMethod) {
+    //console.log('vgtime-开屏页');
+    let obj = JSON.parse(magicJS.response.body);
+    if (body.data == undefined || body.data.ad === undefined) {
+        //console.log("body:" + $response.body);
+        $notification.post(notifiTitle, "vgtime", "data/ad字段为undefined")
+    } else {
+        body.data.ad = null;
+        //console.log('成功')
+    }
+} else if (url.indexOf("api.youxishiguang.com/vgtime-app/api/v2/init/ad.json") != -1 && method == postMethod) {
+    //console.log('vgtime-开屏页');
+    if (body.data == undefined || body.data.ad === undefined) {
+        //console.log("body:" + $response.body);
+        $notification.post(notifiTitle, "vgtime", "data/ad字段为undefined")
+    } else {
+        body.data.ad = null;
+        //console.log('成功')
+    }
+} else if (url.indexOf("app02.vgtime.com:8080/vgtime-app/api/v3/topic/detail") != -1 && method == postMethod) {
+    //console.log('vgtime-内容页');
+    if (body.data == undefined || body.data.ad === undefined) {
+        //console.log("body:" + $response.body);
+        $notification.post(notifiTitle, "vgtime", "data/ad字段为undefined")
+    } else {
+        body.data.ad = null;
+        //console.log('成功')
+    }
 }
