ed23f3d6ab3581ed8fe198724c17845b1ad43080
ATor <atorninjaonline@gmail.com>
2022-02-24 10:58:39 +0800

    update baidu

---
+++ baiduUA.js
@@ -0,0 +1,8 @@
+let url = $request.url;
+let headers = $request.headers;
+if (url.indexOf("baidu.com") !== -1) {
+	if (headers["User-Agent"].indexOf("iPhone") !== -1)
+		headers["User-Agent"] =
+			"Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/16C50 Quark/604.1 T7/10.3 SearchCraft/2.6.3 (Baidu; P1 8.0.0)";
+}
+$done({ headers });
