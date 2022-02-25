c02035daaf20ad6bc397bf2e9d1a9f2a6073a17c
ATor <atorninjaonline@gmail.com>
2022-02-25 18:14:04 +0800

    update baiduUA.js

--- baiduUA.js
+++ baiduUA.js
@@ -3,6 +3,6 @@ let headers = $request.headers;
 if (url.indexOf("baidu.com") !== -1) {
 	if (headers["User-Agent"].indexOf("iPhone") !== -1)
 		headers["User-Agent"] =
-			"Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/16C50 Quark/604.1 T7/10.3 SearchCraft/2.6.3 (Baidu; P1 8.0.0)";
+			"Mozilla/5.0 (Linux; Android 8.0.0; SM-N9500 Build/R16NW; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/63.0.3239.83 Mobile Safari/537.36 T7/10.13 baiduboxapp/10.13.0.11 (Baidu; P1 8.0.0)";
 }
 $done({ headers });
