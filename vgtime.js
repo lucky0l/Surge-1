aae279207322b34946c58ec546c9e5160fce2000
ATor <atorninjaonline@gmail.com>
2022-02-28 10:24:14 +0800

    update vgtime

--- vgtime.js
+++ vgtime.js
@@ -31,6 +31,19 @@ if (url.indexOf("app02.vgtime.com:8080/vgtime-app/api/v2/init/ad.json") != -1 &&
         body.data.ad = null;
         //console.log('成功')
     }
+} else if (url.indexOf("api-acess.pangolin-sdk-toutiao.com/api/ad/union/sdk") != -1 && method == postMethod) {
+    //console.log('穿山甲-get_ads');
+    if (body.message === undefined) {
+        //console.log("body:" + $response.body);
+        if (body.status_code === undefined) {
+            $notification.post(notifiTitle, "穿山甲", "message/status_code字段错误");
+        } else {
+            //console.log('广告为空');
+        }
+    } else {
+        body.message = null;
+        //console.log('成功')
+    }
 } else {
     $notification.post(notifiTitle, "路径/请求方法匹配错误:", method + "," + url)
 }
