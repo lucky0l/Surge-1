db01497c19ce5bb4a40c513b66701bcf16071b10
ATor <atorninjaonline@gmail.com>
2022-03-15 19:23:45 +0800

    update bilibili

--- BilibiliRegion.js
+++ BilibiliRegion.js
@@ -77,7 +77,7 @@ let run = EnvInfo();
 
 async function SwitchRegion(play) {
 	const Group = $.read('BiliArea_Policy') || 'Bilibili'; //Your blibli policy group name.
-	const CN = $.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
+	const CN = $.read('BiliArea_CN') || 'Direct'; //Your China sub-policy name.
 	const TW = $.read('BiliArea_TW') || 'TW'; //Your Taiwan sub-policy name.
 	const HK = $.read('BiliArea_HK') || 'HK'; //Your HongKong sub-policy name.
 	const DF = $.read('BiliArea_DF') || 'DF'; //Sub-policy name used after region is blocked(e.g. url 404)
