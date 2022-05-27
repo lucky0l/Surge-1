8679247f4e5b9d19bae9482e7676e6cd01a7fb52
ATor <atorninjaonline@gmail.com>
2022-05-27 10:55:56 +0800

    JD

--- jds.js
+++ jds.js
@@ -1,6 +1,9 @@
-var body = $response.body;
-var obj = JSON.parse(body);
-obj.data.contentList = [
-
-  ];
-$done({body: JSON.stringify(obj)});
+let body = JSON.parse($response.body);
+  body.floors = body.floors.filter(function(item) {
+    if (item.mId == "commonBanner" || item.mId == "myownerfloor_wodezhuanshu" || item.mId == "recommendfloor"|| item.mId == "horizontalCard") {
+      return false;
+    }
+    return true;
+  });
+if(body.floors.hasOwnProperty('1')){body['floors']['2']['data']['logisticsInfo'] = { }};
+$done({body: JSON.stringify(body)});
